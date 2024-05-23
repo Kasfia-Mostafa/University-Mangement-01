d1cc2ad3122b9c7e5dd3e3d81b9f99eae7bcca16
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2024-05-23 20:43:48 +0600

    nstall Prettier,Ts-Node-Dev

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import config from './app/config';
 import mongoose from 'mongoose';
 import app from './app';
 
+
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string,);
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
@@ -12,5 +13,4 @@ async function main() {
     console.log(err);
   }
 }
-
 main();
